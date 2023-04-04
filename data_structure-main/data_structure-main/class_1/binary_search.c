afe00117ab57ec0f681a058d14597ba122be597b
Md Taofick <107477685+taofick221@users.noreply.github.com>
2023-04-04 15:14:36 +0600

    Add files via upload

---
+++ data_structure-main/data_structure-main/class_1/binary_search.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+int main()
+{
+	int a[]={1,2,4,5,67,89,100};
+	int item=4;
+	
+	int left,right,middle;
+	left=0;
+	right=6;
+	
+	while(left<=right){
+		
+		middle=(left+right)/2;
+		if(a[middle]==item){
+			
+			printf("Item found at index:%d\n",middle);
+			return 0;
+			
+		}
+		else if(a[middle]<item){
+			
+			left=middle+1;
+			
+		}
+		else{
+			
+			right=middle-1;
+		}
+	}
+	
+	printf("Item not found\n");
+	
+	
+	return 0;
+}
